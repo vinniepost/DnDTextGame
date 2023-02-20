9cca2c9033a39302ca2e24ff64359f4ff4a41ffe
vinnie post <vinnie.post1@gmail.com>
2023-02-20 22:24:20 +0100

    test

---
+++ main.py
@@ -0,0 +1,16 @@
+# imports
+
+
+# vars
+
+
+# objects
+class TestObject:
+    def __init__(self, test="test"):
+        self.test = test
+
+    def TestFunctie(input):
+        print(input)
+
+
+# code
